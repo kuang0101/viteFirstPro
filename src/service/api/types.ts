e3e81a868cc6c158b276452ef296e4caaf1c4915
kuanglongqin <2644629326@qq.com>
2022-04-22 14:47:54 +0800

    完成登录请求的简单封装

---
+++ src/service/api/types.ts
@@ -0,0 +1,9 @@
+// qingqiu
+export interface ILoginParams {
+  userName: string;
+  passWord: string | number;
+}
+
+export interface ILoginApi {
+  login: (params: ILoginParams) => Promise<any>;
+}
